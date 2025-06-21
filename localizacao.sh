da8987250f660aa9f244adc07690f0fa79bcf813
Ricardo Moreira <moanrial@gmail.com>
2025-06-21 16:59:33 +0100

    tudo em minusculas

--- localizacao.sh
+++ localizacao.sh
@@ -1,12 +1,12 @@
-# Para corrigir e adicionar localização ao gnome-weather
+# para corrigir e adicionar localização ao gnome-weather
 
 function localizacao_fix() {
 clear
-log_section "A ADICIONAR LOCALIZAÇÃO AO GNOME-WEATHER"
-wget -q -O ./tmp/gnome-weather-fix.sh "https://gist.github.com/dotbanana/1dc4d95d644ce72ab8741d6886b86acc/raw/9e12907ef036193fef4176c4ea0f396fa3f57321/add-location-to-gnome-weather.sh"
+log_section "a adicionar localização ao gnome-weather"
+wget -q -o ./tmp/gnome-weather-fix.sh "https://gist.github.com/dotbanana/1dc4d95d644ce72ab8741d6886b86acc/raw/9e12907ef036193fef4176c4ea0f396fa3f57321/add-location-to-gnome-weather.sh"
 chmod +x ./tmp/gnome-weather-fix.sh
 ./tmp/gnome-weather-fix.sh
-echo "${GREEN}FINALIZADO${RESET} ✅"
+echo "${green}finalizado${reset} ✅"
 sleep 1.5
 clear
 }
--- montar_hdd.sh
+++ montar_hdd.sh
@@ -1,29 +1,29 @@
-# Montar HDD
+# montar hdd
 
 function montar_hdd() {
 clear
-log_section "A MONTAR DISCO DE RESTAURO"
-MOUNT_POINT="/mnt/Restauro"
-UUID="f794f76e-0e02-4c91-89ce-61feaccb35c7"
-# Cria o ponto de montagem, se não existir
-if [ ! -d "$MOUNT_POINT" ]; then
-echo "A CRIAR PONTO DE MONTAGEM EM $MOUNT_POINT..."
-sudo mkdir -p "$MOUNT_POINT"
+log_section "a montar disco de restauro"
+mount_point="/mnt/restauro"
+uuid="f794f76e-0e02-4c91-89ce-61feaccb35c7"
+# cria o ponto de montagem, se não existir
+if [ ! -d "$mount_point" ]; then
+echo "a criar ponto de montagem em $mount_point..."
+sudo mkdir -p "$mount_point"
 sleep 1.5
-echo "${GREEN}FINALIZADO${RESET} ✅"
+echo "${green}finalizado${reset} ✅"
 clear
 fi
-# Verifica se já está montado
-if ! mountpoint -q "$MOUNT_POINT"; then
-echo "A MONTAR O DISCO DE RESTAURO..."
-sudo mount -U "$UUID" "$MOUNT_POINT"
-echo "DISCO MONTADO COM SUCESSO EM $MOUNT_POINT"
-echo "${GREEN}FINALIZADO${RESET} ✅"
+# verifica se já está montado
+if ! mountpoint -q "$mount_point"; then
+echo "a montar o disco de restauro..."
+sudo mount -u "$uuid" "$mount_point"
+echo "disco montado com sucesso em $mount_point"
+echo "${green}finalizado${reset} ✅"
 sleep 1.5
 clear
 else
-echo "O DISCO DE RESTAURO JÁ ESTÁ MONTADO EM $MOUNT_POINT."
-echo "${GREEN}FINALIZADO${RESET} ✅"
+echo "o disco de restauro já está montado em $mount_point."
+echo "${green}finalizado${reset} ✅"
 sleep 1.5
 clear
 fi
