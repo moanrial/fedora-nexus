486d8c12728de7cac2f2ea4b1adad08f458e4707
moanrial <moanrial@gmail.com>
2025-06-21 15:56:24 +0100

    Update localizacao.sh

--- localizacao.sh
+++ localizacao.sh
@@ -1,6 +1,6 @@
 # Para corrigir e adicionar localização
 
-function adicionar_localizacao_fix() {
+function localizacao_fix() {
 clear
 log_section "A ADICIONAR LOCALIZAÇÃO AO GNOME-WEATHER"
 wget -q -O ./tmp/gnome-weather-fix.sh "https://gist.github.com/dotbanana/1dc4d95d644ce72ab8741d6886b86acc/raw/9e12907ef036193fef4176c4ea0f396fa3f57321/add-location-to-gnome-weather.sh"
