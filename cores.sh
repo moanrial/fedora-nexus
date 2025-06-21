8a8308a42dd3c7eead7187f2b95b654d60024841
Ricardo Moreira <moanrial@gmail.com>
2025-06-21 16:55:49 +0100

    tudo em minusculo

--- cores.sh
+++ cores.sh
@@ -1,9 +1,8 @@
 # cores.sh - Códigos ANSI para formatação de texto no terminal
 
-YELLOW=$'\033[1;33m'
-RESET=$'\033[0m'
-RED=$'\033[0;31m'
-NEGRITO=$'\033[1m'
-GREEN=$'\033[1;32m'
-CIANO=$'\033[36m'
-
+yellow=$'\033[1;33m'
+reset=$'\033[0m'
+red=$'\033[0;31m'
+negrito=$'\033[1m'
+green=$'\033[1;32m'
+ciano=$'\033[36m'
