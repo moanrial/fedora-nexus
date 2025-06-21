7a4b7de95279679d08157885c35cbda407df973e
moanrial <moanrial@gmail.com>
2025-06-21 21:20:41 +0100

    Add files via upload

---
+++ scripts/localizacao.sh
@@ -0,0 +1,11 @@
+# Corrigir e adicionar localização ao Gnome-Weather
+
+function localizacao_fix() {
+clear
+log_section "A adicionar localização ao Gnome-Weather."
+chmod +x ./tmp/add-location-to-gnome-weather.sh
+./tmp/add-location-to-gnome-weather.sh
+sucesso "Finalizado."
+sleep 1.5
+clear
+}
