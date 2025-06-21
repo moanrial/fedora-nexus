a1552620491c3f256f0f72425b82c02885d71650
moanrial <moanrial@gmail.com>
2025-06-21 14:42:55 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,5 +1,16 @@
 # utils.sh - Funções auxiliares reutilizáveis
 
+function verificar_dependencias() {
+DEPENDENCIAS=("ping" "sudo" "curl" "tee")
+for cmd in "${DEPENDENCIAS[@]}"; do
+if ! command -v "$cmd" > /dev/null 2>&1; then
+echo "${NEGRITO}${RED}ERRO: O COMANDO '$CMD' NÃO ESTÁ INSTALADO.${RESET}"
+exit 1
+fi
+done
+echo "${NEGRITO}${GREEN}TODAS AS DEPENDÊNCIAS ESTÃO SATISFEITAS.${RESET}"
+}
+
 ### --- VERIFICAR CONECTIVIDADE --- ###
 function verificar_ligacao() {
 echo "${NEGRITO}${YELLOW}A VERIFICAR CONECTIVIDADE COM A INTERNET...${RESET}"
@@ -20,15 +31,4 @@ while true; do sleep 60; sudo -v; done &
 SUDO_KEEP_ALIVE_PID=$!
 trap '[ -n "$SUDO_KEEP_ALIVE_PID" ] && kill "$SUDO_KEEP_ALIVE_PID"' EXIT
 clear
-}
-
-function verificar_dependencias() {
-DEPENDENCIAS=("ping" "sudo" "curl" "tee")
-for cmd in "${DEPENDENCIAS[@]}"; do
-if ! command -v "$cmd" > /dev/null 2>&1; then
-echo "${NEGRITO}${RED}ERRO: O COMANDO '$CMD' NÃO ESTÁ INSTALADO.${RESET}"
-exit 1
-fi
-done
-echo "${NEGRITO}${GREEN}TODAS AS DEPENDÊNCIAS ESTÃO SATISFEITAS.${RESET}"
 }
