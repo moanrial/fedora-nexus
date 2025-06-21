9ce7a8328e0c171cbc8ea7cba7204b831418e0d0
moanrial <moanrial@gmail.com>
2025-06-21 18:46:57 +0100

    Delete cores.sh

--- cores.sh
+++
@@ -1,8 +0,0 @@
-# cores.sh - Códigos ANSI para formatação de texto no terminal
-
-yellow=$'\033[1;33m'
-reset=$'\033[0m'
-red=$'\033[0;31m'
-negrito=$'\033[1m'
-green=$'\033[1;32m'
-ciano=$'\033[36m'
