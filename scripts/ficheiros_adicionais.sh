5fa674e6a0c12874a4aa139fdc936675f893a4f8
moanrial <moanrial@gmail.com>
2025-06-22 17:31:32 +0100

    Update ficheiros_adicionais.sh

--- scripts/ficheiros_adicionais.sh
+++ scripts/ficheiros_adicionais.sh
@@ -18,7 +18,6 @@ ficheiros=(
 "https://droidcam.app/go/droidCam.client.setup.rpm"
 "https://aplicacoes.autenticacao.gov.pt/apps/pteid-mw-pcsclite-2.3.flatpak"
 "https://aplicacoes.autenticacao.gov.pt/plugin/plugin-autenticacao-gov_fedora.rpm"
-"https://gist.github.com/dotbanana/1dc4d95d644ce72ab8741d6886b86acc/raw/9e12907ef036193fef4176c4ea0f396fa3f57321/add-location-to-gnome-weather.sh"
 )
 
 for url in "${ficheiros[@]}"; do
