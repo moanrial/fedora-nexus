a209d30bb0e04a70cfb37cdc7d94fc549f78149b
moanrial <moanrial@gmail.com>
2025-06-22 12:23:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ transferir_auxiliares
 if [[ "${1:-}" == "--silent" || "${1:-}" == "--auto" ]]; then
   manter_sudo_ativo &  # Garante sudo enquanto corre
   atualizar_sistema
-  instalar_pacotes_do_utilizador
+  instalar_pacotes
   instalar_flatpaks
   instalar_ficheiros_adicionais
   info "Instalação completa."
