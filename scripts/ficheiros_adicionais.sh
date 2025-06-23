cf89bde9d7d127d68b78e28b04febf25ae8d3e81
moanrial <moanrial@gmail.com>
2025-06-23 18:32:32 +0100

    Update ficheiros_adicionais.sh

--- scripts/ficheiros_adicionais.sh
+++ scripts/ficheiros_adicionais.sh
@@ -59,4 +59,12 @@ sudo dnf install -y "$TMP_DIR/plugin-autenticacao-gov_fedora.rpm"
 sucesso "Ficheiros adicionais instalados com sucesso."
 sleep 1.5
 
+# Instalar VisualStudioCode
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update
+sudo dnf install code
+
 }
